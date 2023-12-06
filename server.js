0c237be6d6efa8d01dbec98c26c1c5c3bd653f76
JohnnyBackus <john.p.backus@gmail.com>
2023-12-05 16:42:43 -0800

    server updates

--- server.js
+++ server.js
@@ -4,7 +4,23 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-app.use( cors() );
+app.use(cors());
 const PORT = process.env.PORT || 3000;
-console.log('it works');
+const weatherDataArray = require('./data/weather.json');
 
+app.listen(
+    PORT, () => console.log(`listening on ${PORT}`)
+);
+
+console.log (weatherDataArray[0].city_name)
+app.get('/', (request, response) => {
+    response.send("Hellooooo");
+});
+
+app.get("/weather", (request, response) => {
+    let city = request.query.type;
+    if( weatherDataArray[type] ) {
+        response.json(weatherData[type])
+    } else {
+        throw new Error("No Such List")
+    }
